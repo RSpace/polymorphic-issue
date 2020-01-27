648de2c4afe54a1e1dff767c7b980bb905e50bad
Casper Fabricius <me@casperfabricius.com>
2020-01-27 12:36:22 +0100

    Use the "polymorphic_name" method to explicitly tell ActiveRecord what model name to use in the has_many :through join

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,5 +2,9 @@ class Organization < ApplicationRecord
   self.table_name = 'organisations'
 
   has_many :roles, as: :access_to
-  has_many :users, through: :roles, source: :access_to, source_type: "Organisation"
+  has_many :users, through: :roles
+
+  def self.polymorphic_name
+    "Organisation"
+  end
 end
